3e20aaafb4a6dff3575fbabf40c5bb04b7707166
Inna Groznaya <inna2011@tut.by>
2023-09-23 17:14:24 +0300

    feat: add select and partly styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { SelectCustom } from '@/components/ui/select'
+
 export function App() {
-  return <div>Hello</div>
+  const items = [
+    { label: 'lab1', value: 'val1' },
+    { label: 'lab2', value: 'val2' },
+    { label: 'lab3', value: 'val3', disabled: true },
+  ]
+
+  const callback = (value: string) => {
+    console.log(value)
+  }
+
+  return (
+    <div>
+      Hello
+      <div style={{ margin: '100px' }}>
+        <SelectCustom items={items} callback={callback} disabled={true} />
+      </div>
+    </div>
+  )
 }
---
+++ src/components/ui/select/select.tsx
@@ -0,0 +1,58 @@
+import { FC, useState } from 'react'
+
+import * as Select from '@radix-ui/react-select'
+import { SelectProps } from '@radix-ui/react-select'
+
+import vectorDown from './assets/icons/VectorDown.svg'
+import vectorUp from './assets/icons/VectorUp.svg'
+import s from './select.module.scss'
+
+type OptionType = { label: string; value: string; disabled?: boolean }
+
+type SelectCustomProps = SelectProps & {
+  items: OptionType[]
+  callback?: (value: string) => void
+}
+
+export const SelectCustom: FC<SelectCustomProps> = ({ items, callback, disabled }) => {
+  // const countries = { france: '🇫🇷', 'united-kingdom': '🇬🇧', spain: '🇪🇸' }
+  const [label, setLabel] = useState(items[0].label)
+  const [isOpen, setIsOpen] = useState(false)
+  const onValueChangeHandler = (value: string) => {
+    setLabel(value)
+    if (callback) {
+      callback(value)
+    }
+  }
+
+  const arrowImg = <img src={isOpen ? vectorUp : vectorDown} />
+
+  return (
+    <div className={`${s.wrapper} ${disabled ? s.disabled : ''}`}>
+      <Select.Root
+        value={label}
+        onValueChange={onValueChangeHandler}
+        onOpenChange={() => setIsOpen(!isOpen)}
+        disabled={disabled}
+      >
+        <Select.Trigger className={s.selectTrigger}>
+          <Select.Value aria-label={label}>{label}</Select.Value>
+          <Select.Icon className={s.selectIcon}>{arrowImg}</Select.Icon>
+        </Select.Trigger>
+        <Select.Portal className={s.selectPortal}>
+          <Select.Content position="popper" className={s.selectContent}>
+            <Select.Viewport className={s.selectViewport}>
+              {items.map((el, index) => {
+                return (
+                  <Select.Item value={el.label} key={index} className={s.selectItem}>
+                    <Select.ItemText>{el.value}</Select.ItemText>
+                  </Select.Item>
+                )
+              })}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </div>
+  )
+}
