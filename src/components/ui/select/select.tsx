07908736a84e575dae6d7562d2cd3829f2646d94
Inna Groznaya <inna2011@tut.by>
2023-09-24 13:02:14 +0300

    just for help

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -3,8 +3,9 @@ import { FC, useState } from 'react'
 import * as Select from '@radix-ui/react-select'
 import { SelectProps } from '@radix-ui/react-select'
 
-import vectorDown from './assets/icons/VectorDown.svg'
-import vectorUp from './assets/icons/VectorUp.svg'
+import iconDown from '../../../assets/images/icons/iconDown.svg'
+import iconUp from '../../../assets/images/icons/iconUp.svg'
+
 import s from './select.module.scss'
 
 type OptionType = { label: string; value: string; disabled?: boolean }
@@ -25,12 +26,20 @@ export const SelectCustom: FC<SelectCustomProps> = ({ items, callback, disabled 
     }
   }
 
-  const arrowImg = <img src={isOpen ? vectorUp : vectorDown} />
+  const arrowImg = <img src={isOpen ? iconUp : iconDown} />
+
+  const selectItems = items.map((el, index) => {
+    return (
+      <Select.Item value={el.label} key={index} className={s.selectItem}>
+        <Select.ItemText>{el.value}</Select.ItemText>
+      </Select.Item>
+    )
+  })
 
   return (
     <div className={s.wrapper}>
       <Select.Root
-        open={true}
+        // open={true}
         value={label}
         onValueChange={onValueChangeHandler}
         onOpenChange={() => setIsOpen(!isOpen)}
@@ -38,19 +47,11 @@ export const SelectCustom: FC<SelectCustomProps> = ({ items, callback, disabled 
       >
         <Select.Trigger className={`${s.selectTrigger} ${disabled ? s.disabled : ''}`}>
           <Select.Value aria-label={label}>{label}</Select.Value>
-          <Select.Icon className={s.selectIcon}>{arrowImg}</Select.Icon>
+          <Select.Icon className={isOpen ? s.arrowUp : s.arrowDown} asChild={true} />
         </Select.Trigger>
         <Select.Portal className={s.selectPortal}>
-          <Select.Content position="popper" className={s.selectContent}>
-            <Select.Viewport className={s.selectViewport}>
-              {items.map((el, index) => {
-                return (
-                  <Select.Item value={el.label} key={index} className={s.selectItem}>
-                    <Select.ItemText>{el.value}</Select.ItemText>
-                  </Select.Item>
-                )
-              })}
-            </Select.Viewport>
+          <Select.Content position="popper" align={'center'} className={s.selectContent}>
+            <Select.Viewport className={s.selectViewport}>{selectItems}</Select.Viewport>
           </Select.Content>
         </Select.Portal>
       </Select.Root>
