da3a4fce870ca2f8133362f726e62e7d47a37150
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-09-27 13:01:31 +0300

    add forwardRed in Card, and card is polymorph

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,16 +1,25 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ElementType, ForwardedRef, forwardRef } from 'react'
 
 import s from './card.module.scss'
 
-export type CardProps = {
-  children?: ReactNode
+export type CardProps<T extends ElementType = 'div'> = {
+  as?: T
   variant?: 'dark' | 'light'
-} & ComponentPropsWithoutRef<'div'>
+} & ComponentPropsWithoutRef<T>
 
-export const Card = ({ children, variant = 'dark', ...rest }: CardProps) => {
-  return (
-    <div className={`${s.card} ${s[variant]}`} {...rest}>
-      {children}
-    </div>
-  )
+const CardPolymorph = <T extends ElementType = 'div'>(
+  props: CardProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof CardProps<T>>,
+  ref: ElementRef<T>
+) => {
+  const { variant = 'dark', as: Tag = 'div', ...rest } = props
+
+  // @ts-ignore expected TS2322
+  return <Tag className={`${s.card} ${s[variant]}`} ref={ref} {...rest}></Tag>
 }
+
+export const Card = forwardRef(CardPolymorph) as <T extends ElementType = 'div'>(
+  props: CardProps<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof CardProps<T>> & {
+      ref?: ForwardedRef<ElementRef<T>>
+    }
+) => ReturnType<typeof CardPolymorph>
