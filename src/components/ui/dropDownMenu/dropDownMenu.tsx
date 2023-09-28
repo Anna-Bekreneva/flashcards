23d02f988e2b174743924f93d8aae28a7566f582
Inna Groznaya <inna2011@tut.by>
2023-09-28 17:27:26 +0300

    add forwardRef to DropDown

--- src/components/ui/dropDownMenu/dropDownMenu.tsx
+++ src/components/ui/dropDownMenu/dropDownMenu.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { DropdownMenuProps } from '@radix-ui/react-dropdown-menu'
@@ -16,52 +16,49 @@ type DropDownMenuPropsType = DropdownMenuProps & {
   onItemSelect?: (event: Event) => void
   triggerIMG: string
   alignType: 'center' | 'start' | 'end'
-}
+} & ComponentPropsWithoutRef<'div'>
 
-export const DropDownMenu: FC<DropDownMenuPropsType> = ({
-  items,
-  onItemSelect,
-  alignType,
-  triggerIMG,
-}) => {
-  const mappedItems = items.map((el, index) => {
+export const DropDownMenu = forwardRef<HTMLDivElement, DropDownMenuPropsType>(
+  ({ items, onItemSelect, alignType, triggerIMG }, ref) => {
+    const mappedItems = items.map((el, index) => {
+      return (
+        <DropdownMenu.Item onSelect={onItemSelect} key={index} className={s.item} ref={ref}>
+          <span className={s.icon}>{el.icon}</span>
+          <span className={s.itemValues}>
+            {el.value}
+            <br />
+            {el.extraValue && <span className={s.extraValue}>{el.extraValue}</span>}
+          </span>
+          <DropdownMenu.Separator
+            className={
+              index === items.length - 1
+                ? `${s.DropdownMenuSeparator} ${s.notDisplayed}`
+                : s.DropdownMenuSeparator
+            }
+          />
+        </DropdownMenu.Item>
+      )
+    })
+
+    const contentExtraClassName =
+      // eslint-disable-next-line no-nested-ternary
+      alignType === 'end' ? s.end : alignType === 'center' ? s.center : s.start
+
     return (
-      <DropdownMenu.Item onSelect={onItemSelect} key={index} className={s.item}>
-        <span className={s.icon}>{el.icon}</span>
-        <span className={s.itemValues}>
-          {el.value}
-          <br />
-          {el.extraValue && <span className={s.extraValue}>{el.extraValue}</span>}
-        </span>
-        <DropdownMenu.Separator
-          className={
-            index === items.length - 1
-              ? `${s.DropdownMenuSeparator} ${s.notDisplayed}`
-              : s.DropdownMenuSeparator
-          }
-        />
-      </DropdownMenu.Item>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>{<img src={triggerIMG} className={s.icon} />}</DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            className={`${s.contentMenu} ${contentExtraClassName}`}
+            onCloseAutoFocus={e => {
+              e.preventDefault()
+            }}
+            align={alignType}
+          >
+            {mappedItems}
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
     )
-  })
-
-  const contentExtraClassName =
-    // eslint-disable-next-line no-nested-ternary
-    alignType === 'end' ? s.end : alignType === 'center' ? s.center : s.start
-
-  return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger>{<img src={triggerIMG} className={s.icon} />}</DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content
-          className={`${s.contentMenu} ${contentExtraClassName}`}
-          onCloseAutoFocus={e => {
-            e.preventDefault()
-          }}
-          align={alignType}
-        >
-          {mappedItems}
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
-  )
-}
+  }
+)
