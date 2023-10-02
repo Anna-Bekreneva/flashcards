c000d3ecda0ebffad412c869084abd7dad7ed320
Anna-Bekreneva <anjabkereneva@gmail.com>
2023-10-02 17:05:37 +0300

    reset app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-import { Button } from '@/components/ui/button'
-import { TabSwitcher } from '@/components/ui/tabSwitcher'
-
 export function App() {
-  const changeTabValueHandler = (value: string) => console.log(value)
-
-  const items = [
-    {
-      value: 'tab-1',
-      triggerText: 'tab-1',
-      content: <div>text 1</div>,
-    },
-    {
-      value: 'tab-2',
-      triggerText: 'tab-2',
-      content: <div>text 2</div>,
-      disabled: true,
-    },
-  ]
-
-  return (
-    <>
-      <div>Hello</div>
-      <Button variant={'tertiary'}>123</Button>
-      <TabSwitcher defaultValue={'tab-1'} items={items} callback={changeTabValueHandler} />
-    </>
-  )
+  return <div>Hello</div>
 }
