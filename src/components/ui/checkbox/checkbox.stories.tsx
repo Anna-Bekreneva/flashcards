655863d73abffc866ee5dc2bd282110ce8293706
Anna-Bekreneva <anjabkereneva@gmail.com>
2023-10-03 16:23:21 +0300

    correct checkbox.stories

---
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+import { action } from '@storybook/addon-actions'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { CheckboxCustom } from './'
+
+const meta = {
+  title: 'Components/Checkbox',
+  component: CheckboxCustom,
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: { type: 'boolean' },
+    checked: { type: 'boolean' },
+    id: { type: 'string' },
+    value: { type: 'string' },
+    onCheckedChange: { description: '(checked: boolean) => void' },
+  },
+} satisfies Meta<typeof CheckboxCustom>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const CheckboxDefault: Story = {
+  render: args => {
+    const [checked, setChecked] = useState(true)
+    const changeChecked = action(`${checked}`)
+    const onCheckedChange = (checked: boolean) => {
+      setChecked(checked)
+      changeChecked()
+    }
+
+    return (
+      <CheckboxCustom
+        {...args}
+        checked={checked}
+        onCheckedChange={onCheckedChange}
+      ></CheckboxCustom>
+    )
+  },
+}
