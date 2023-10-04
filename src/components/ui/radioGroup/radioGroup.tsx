8c555a2b3e812d1694844985dc1fcacb4998e25d
Anna-Bekreneva <anjabkereneva@gmail.com>
2023-10-04 12:36:54 +0300

    correct radioGroup (remove useState, handler, change props and add forwardRef)

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,57 +1,46 @@
-import { FC, useState } from 'react'
+import { ElementRef, forwardRef } from 'react'
 
 import * as RadioGroup from '@radix-ui/react-radio-group'
-import { RadioGroupItemProps, RadioGroupProps } from '@radix-ui/react-radio-group'
+import { RadioGroupProps } from '@radix-ui/react-radio-group'
 
 import s from './radioGroup.module.scss'
 
-type RadioGroupCustomProps = {
+import { TypographyVariant } from '@/common/types/types.ts'
+import { Typography } from '@/components/ui/typography'
+
+type Props = {
   items: RadioButtonType[]
-  defaultValue: string
-  root?: RadioGroupProps
-  callback: (value: string) => void
-}
+} & RadioGroupProps
 
 type RadioButtonType = {
-  radioButton: RadioGroupItemProps
+  value: string
+  disabled?: boolean
   label: string
 }
 
-export const RadioGroupCustom: FC<RadioGroupCustomProps> = ({
-  items,
-  defaultValue,
-  root,
-  callback,
-}) => {
-  const [value, setValue] = useState(defaultValue)
+export const RadioGroupCustom = forwardRef<ElementRef<typeof RadioGroup.Root>, Props>(
+  ({ items, ...props }, ref?) => {
+    const radioItems =
+      items.length &&
+      items.map((item, index) => {
+        return (
+          <label className={`${s.inner} ${item.disabled ? s.disabled : ''}`} key={index}>
+            <RadioGroup.Item
+              className={`${s.radio} ${props.value === item.value ? s.active : ''} `}
+              value={item.value}
+              disabled={item.disabled}
+            />
+            <Typography className={s.name} as={'span'} variant={TypographyVariant.body2}>
+              {item.label}
+            </Typography>
+          </label>
+        )
+      })
 
-  const radioItems =
-    items.length &&
-    items.map((item, index) => {
-      const clickHandler = () => {
-        const newValue = item.radioButton.value
-
-        setValue(newValue)
-        callback(newValue)
-      }
-
-      return (
-        <label className={`${s.inner} ${item.radioButton.disabled ? s.disabled : ''}`} key={index}>
-          <RadioGroup.Item
-            className={`${s.radio} ${value === item.radioButton.value ? s.active : ''} `}
-            {...item.radioButton}
-            value={value}
-            disabled={item.radioButton.disabled}
-            onClick={clickHandler}
-          />
-          <span className={s.name}>{item.label}</span>
-        </label>
-      )
-    })
-
-  return (
-    <RadioGroup.Root className={`${s.root} ${root?.disabled ? s.disabled : ''}`} {...root}>
-      {radioItems}
-    </RadioGroup.Root>
-  )
-}
+    return (
+      <RadioGroup.Root className={s.root} ref={ref} {...props}>
+        {radioItems}
+      </RadioGroup.Root>
+    )
+  }
+)
