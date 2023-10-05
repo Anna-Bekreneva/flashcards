b4587ada66d0f13767e14318060dc04d0a725c66
Anna-Bekreneva <anjabkereneva@gmail.com>
2023-10-05 12:34:42 +0300

    correct select.stories

---
+++ src/components/ui/select/select.stories.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+import { action } from '@storybook/addon-actions'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Select } from './'
+
+const meta = {
+  title: 'Components/Select',
+  component: Select,
+  tags: ['autodocs'],
+  argTypes: {
+    label: { type: 'string' },
+    id: { type: 'string' },
+    name: { type: 'string' },
+    disabled: { type: 'boolean' },
+    onValueChange: { description: '(value: string) => void' },
+  },
+} satisfies Meta<typeof Select>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SelectDefault: Story = {
+  render: args => {
+    const items = [
+      { label: 'label1', value: 'value1' },
+      { label: 'label2', value: 'value2' },
+      { label: 'label3', value: 'value3' },
+    ]
+
+    const [value, setVaue] = useState('value1')
+    const onChange = (value: string) => {
+      action(value)()
+      setVaue(value)
+    }
+
+    return <Select {...args} items={items} value={value} onValueChange={onChange} />
+  },
+}
