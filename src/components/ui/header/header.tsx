73ced7cfeb390679d11ff3e24fff9a870cb1b982
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-10-17 13:01:19 +0300

    correct header (use new dropDownMenu, change props, change classes, use Typography)

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -2,62 +2,71 @@ import { forwardRef } from 'react'
 
 import s from './header.module.scss'
 
-import { EditIcon } from '@/assets/iconsComponents/edit.tsx'
-import { IncubatorLogo } from '@/assets/iconsComponents/incubatorLogo.tsx'
+import { PersonIcon } from '@/assets/iconsComponents/person.tsx'
 import { SignOutIcon } from '@/assets/iconsComponents/signOut.tsx'
-import { StartIcon } from '@/assets/iconsComponents/start.tsx'
-import userPhoto from '@/assets/images/userPhoto.png'
+import logo from '@/assets/images/logo.svg'
+import userPhoto from '@/assets/images/user.png'
+import { TypographyVariant } from '@/common/types/types.ts'
 import { Button } from '@/components/ui/button'
-import { DropDownMenu, ItemType } from '@/components/ui/dropDownMenu'
+import { DropDownMenu } from '@/components/ui/dropDownMenu'
+import { DropDownItem } from '@/components/ui/dropDownMenu/dropDownItem.tsx'
+import { Typography } from '@/components/ui/typography'
 
 type Props = {
-  isLoggedIn: boolean
-  triggerIMG?: string
   userName?: string
+  className?: string
+  userEmail?: string
 }
 
-const Header = forwardRef<HTMLHeadElement, Props>(
-  ({ isLoggedIn, userName, triggerIMG = userPhoto }, ref) => {
-    const items: ItemType[] = [
-      {
-        icon: (
-          <img src={userPhoto} style={{ width: '36px', height: '36px', borderRadius: '50%' }} />
-        ),
-        extraValue: 'www@bbfghfdhfjgfjgfhjhgvv',
-        value: 'Ivan',
-      },
-      {
-        icon: <EditIcon />,
-        value: 'edit',
-      },
-      {
-        icon: <StartIcon />,
-        value: 'learn',
-      },
-      {
-        icon: <SignOutIcon />,
-        value: 'sign out',
-      },
-    ]
-
-    return (
-      <header ref={ref} className={s.header}>
-        <div className={`container ${s.wrapper}`}>
-          <a href={'#'} className={s.logo}>
-            <IncubatorLogo />
-          </a>
-          {isLoggedIn ? (
-            <span className={s.interactElements}>
-              {userName && <span className={s.userName}>{userName}</span>}
-              <DropDownMenu items={items} triggerIMG={triggerIMG} alignType={'end'} />
-            </span>
-          ) : (
-            <Button>{'Sign In'}</Button>
-          )}
-        </div>
-      </header>
-    )
-  }
-)
+const Header = forwardRef<HTMLHeadElement, Props>(({ userName, userEmail, className }, ref) => {
+  return (
+    <header ref={ref} className={`${s.header} ${className ? className : ''}`}>
+      <div className={`container ${s.wrapper}`}>
+        <a className={s.logo} href={'#'}>
+          <img src={logo} alt="logo" width={156} height={36} />
+        </a>
+        {userName ? (
+          <span className={s.userInfo}>
+            {userName && (
+              <Typography className={s.name} variant={TypographyVariant.subtitle1} as={'span'}>
+                {userName}
+              </Typography>
+            )}
+            <DropDownMenu
+              className={s.dropdown}
+              trigger={
+                <button type="button">
+                  <img className={s.ava} src={userPhoto} alt={userName} width={36} height={36} />
+                </button>
+              }
+            >
+              <DropDownItem className={s.dropdownItem}>
+                <div className={s.dropdownItem}>
+                  <img className={s.ava} src={userPhoto} alt={userName} width={36} height={36} />
+                  <div className={s.dropdownInfo}>
+                    <Typography variant={TypographyVariant.subtitle2} as={'span'}>
+                      {userName}
+                    </Typography>
+                    <Typography className={s.email} variant={TypographyVariant.caption} as={'span'}>
+                      {userEmail}
+                    </Typography>
+                  </div>
+                </div>
+              </DropDownItem>
+              <DropDownItem className={s.dropdownItem}>
+                <PersonIcon /> My Profile
+              </DropDownItem>
+              <DropDownItem className={s.dropdownItem}>
+                <SignOutIcon /> Sign Out
+              </DropDownItem>
+            </DropDownMenu>
+          </span>
+        ) : (
+          <Button>{'Sign In'}</Button>
+        )}
+      </div>
+    </header>
+  )
+})
 
 export default Header
