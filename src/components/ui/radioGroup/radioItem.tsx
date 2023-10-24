bd8841062e34f2b1c7f9cb4876318ac1551e5dc8
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-10-24 12:47:50 +0300

    change radioItem

--- src/components/ui/radioGroup/radioItem.tsx
+++ src/components/ui/radioGroup/radioItem.tsx
@@ -8,26 +8,32 @@ import s from './radioGroup.module.scss'
 import { TypographyVariant } from '@/common/types/types.ts'
 import { Typography } from '@/components/ui/typography'
 
-type Props = RadioGroupItemProps & {
+type Props = {
   label: string
-}
+} & Omit<RadioGroupItemProps, 'id'>
 
-export const RadioItem = forwardRef<HTMLLabelElement, Props>(
-  ({ value, disabled, label, ...props }, ref?) => {
+export const RadioItem = forwardRef<HTMLDivElement, Props>(
+  ({ value, label, disabled, ...props }, ref?) => {
     const innerClassName = `${s.inner} ${disabled ? s.disabled : ''}`
 
     return (
-      <label className={innerClassName} ref={ref}>
+      <div className={innerClassName} ref={ref}>
         <RadixRadioGroup.Item
           className={`${s.radio} ${value === value ? s.active : ''} `}
           value={value}
           disabled={disabled}
+          id={value}
           {...props}
         />
-        <Typography className={s.name} as={'span'} variant={TypographyVariant.body2}>
+        <Typography
+          className={s.name}
+          as={'label'}
+          variant={TypographyVariant.body2}
+          htmlFor={value}
+        >
           {label}
         </Typography>
-      </label>
+      </div>
     )
   }
 )
