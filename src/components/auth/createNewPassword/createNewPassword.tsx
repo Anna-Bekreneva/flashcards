9c57a44403fbabd215ab8ba45666de436787a019
Inna Groznaya <inna2011@tut.by>
2023-11-16 23:19:26 +0300

    feat: add createNewPassword Component

---
+++ src/components/auth/createNewPassword/createNewPassword.tsx
@@ -0,0 +1,53 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button, Card, Typography } from '../../ui'
+
+import s from './createNewPassword.module.scss'
+
+import { ControlledTextField } from '@/components'
+
+const schema = z.object({
+  password: z.string().min(1, 'Please enter password'),
+})
+
+type FormType = z.infer<typeof schema>
+
+type Props = {
+  onSubmit: (data: FormType) => void
+}
+
+export const CreateNewPassword = (props: Props) => {
+  const { control, handleSubmit } = useForm<FormType>({
+    defaultValues: {
+      password: '',
+    },
+    resolver: zodResolver(schema),
+  })
+
+  const handleFormSubmit = handleSubmit(props.onSubmit)
+
+  return (
+    <Card className={s.card}>
+      <Typography className={s.title} variant={'large'}>
+        Create new password
+      </Typography>
+      <form onSubmit={handleFormSubmit}>
+        <ControlledTextField
+          control={control}
+          name={'password'}
+          label={'Password'}
+          type={'password'}
+          className={s.input}
+        />
+        <Typography className={s.info} variant={'caption'}>
+          Create new password and we will send you further instructions to email
+        </Typography>
+        <Button fullWidth type={'submit'}>
+          Create new password
+        </Button>
+      </form>
+    </Card>
+  )
+}
