bde306bcc438e6045178448dff27824b047a2289
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-12-07 17:16:29 +0300

    add deleteDeckModal

---
+++ src/components/decks/deleteDeckModal/deleteDeckModal.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react'
+
+import { TypographyVariant } from '@/common'
+import { DialogButtons, Modal, Typography } from '@/components'
+import s from '@/pages/decksPage/decks.module.scss'
+import { useDeleteDeckMutation } from '@/services'
+
+type Props = {
+  isOpen: boolean
+  onOpenChange: () => void
+  title: string
+  nameDeleteDeck: string
+  idDeleteDeck: string
+}
+export const DeleteDeckModal: FC<Props> = ({
+  isOpen,
+  onOpenChange,
+  title,
+  nameDeleteDeck,
+  idDeleteDeck,
+}) => {
+  const [deleteDeck] = useDeleteDeckMutation()
+
+  return (
+    <Modal title={title} isOpen={isOpen} className={s.modal} onOpenChange={onOpenChange}>
+      <div className={s.modalWrapper}>
+        <div className={s.modalContent}>
+          <Typography>
+            Do you really want to remove
+            <Typography variant={TypographyVariant.subtitle2} as={'span'}>
+              {` ${nameDeleteDeck}? `}
+            </Typography>
+            All cards will be deleted.
+          </Typography>
+        </div>
+        <DialogButtons
+          cancelHandler={onOpenChange}
+          agreeText={'Delete Pack'}
+          agreeHandler={() => {
+            deleteDeck(idDeleteDeck)
+            onOpenChange()
+          }}
+        />
+      </div>
+    </Modal>
+  )
+}
