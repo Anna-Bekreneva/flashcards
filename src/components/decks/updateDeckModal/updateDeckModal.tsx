261cee5a9cc51f243cddfe5ca3d23949153007c9
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-12-08 13:06:12 +0300

    correct updateDeckModal (add opportunity to change cover)

--- src/components/decks/updateDeckModal/updateDeckModal.tsx
+++ src/components/decks/updateDeckModal/updateDeckModal.tsx
@@ -1,50 +1,82 @@
-import { FC } from 'react'
+import { ChangeEvent, FC, useRef, useState } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-import { ControlledCheckbox, ControlledTextField, DialogButtons, Modal } from '@/components'
+import { ImageIcon } from '@/assets/iconsComponents'
+import { ButtonVariant } from '@/common'
+import { Button, ControlledCheckbox, ControlledTextField, DialogButtons, Modal } from '@/components'
 import s from '@/pages/decksPage/decks.module.scss'
 import { useUpdateDeckMutation } from '@/services'
 
 type Props = {
-  title: string
+  currentDeck?: { name?: string; isPrivate?: boolean; cover?: string }
   id: string
+  title: string
   openChangeHandler: () => void
-} & UpdateDeckSchemaType
-
-const UpdateDeckSchema = (name: string, cover: string, isPrivate: boolean) => {
-  return z.object({
-    name: z
-      .string()
-      .min(3, 'name must be longer than or equal to 3 characters')
-      .max(30, 'name must be shorter than or equal to 30 characters')
-      .default(name),
-    cover: z.string().optional().default(cover),
-    private: z.boolean().optional().default(isPrivate),
-  })
 }
 
-type UpdateDeckSchemaType = z.infer<ReturnType<typeof UpdateDeckSchema>>
+const UpdateDeckSchema = z.object({
+  name: z
+    .string()
+    .min(3, 'name must be longer than or equal to 3 characters')
+    .max(30, 'name must be shorter than or equal to 30 characters'),
+  isPrivate: z.boolean(),
+})
+
+type UpdateDeckSchemaType = z.infer<typeof UpdateDeckSchema>
 
 export const UpdateDeckModal: FC<Props> = ({
-  name,
-  cover,
-  private: isPrivate,
+  currentDeck = { name: '', isPrivate: false, cover: '' },
   id,
   title,
   openChangeHandler,
 }) => {
   const [updatePack] = useUpdateDeckMutation()
+
+  const { handleSubmit, control, formState } = useForm<UpdateDeckSchemaType>({
+    defaultValues: { name: currentDeck.name, isPrivate: currentDeck.isPrivate },
+    resolver: zodResolver(UpdateDeckSchema),
+  })
+
+  const [cover, setCover] = useState<File | undefined>(undefined)
+  // const [localCover, setLocalCover] = useState(currentDeck.cover)
+  const [localCover, setLocalCover] = useState(currentDeck.cover)
   const submitHandler = (data: UpdateDeckSchemaType) => {
-    updatePack({ ...data, id, isPrivate })
+    updatePack({ ...data, cover, id })
     openChangeHandler()
   }
 
-  const { handleSubmit, control, formState } = useForm<UpdateDeckSchemaType>({
-    resolver: zodResolver(UpdateDeckSchema(name, cover, isPrivate)),
-  })
+  const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length) {
+      const file = e.target.files[0]
+
+      if (file.size < 4000000) {
+        const formData = new FormData()
+
+        formData.append('cover', file)
+        setCover(file)
+
+        const blob = new Blob([file], { type: 'image/jpeg' })
+
+        const downloadUrl = window.URL.createObjectURL(blob)
+
+        setLocalCover(downloadUrl)
+      } else {
+        alert('Файл слишком большого размера')
+      }
+    }
+  }
+
+  const errorHandler = () => {
+    alert('Кривая картинка')
+  }
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const selectFileHandler = () => {
+    inputRef && inputRef.current?.click()
+  }
 
   return (
     <Modal className={s.modal} title={title} isOpen={!!id} onOpenChange={openChangeHandler}>
@@ -54,19 +86,24 @@ export const UpdateDeckModal: FC<Props> = ({
             className={s.modalInput}
             label={'Name Pack'}
             type={'text'}
-            placeholder={name}
             name={'name'}
             control={control}
           />
-          <ControlledTextField
-            className={s.modalInput}
-            label={'Name Pack'}
-            type={'file'}
-            placeholder={cover}
-            name={'cover'}
-            control={control}
+          {localCover && (
+            <img className={s.img} src={localCover} onError={errorHandler} alt="cover" />
+          )}
+          <Button variant={ButtonVariant.secondary} onClick={selectFileHandler} type={'button'}>
+            <ImageIcon />
+            Upload Cover
+          </Button>
+          <input
+            style={{ display: 'none' }}
+            ref={inputRef}
+            type="file"
+            accept={'image/*'}
+            onChange={uploadHandler}
           />
-          <ControlledCheckbox label={'Private pack'} name={'private'} control={control} />
+          <ControlledCheckbox label={'Private pack'} name={'isPrivate'} control={control} />
         </div>
         <DialogButtons
           cancelHandler={openChangeHandler}
