bf4d7405eeb24bd61738581130a978b22d7ca24f
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-03-06 11:33:01 +0300

    correct personalInformation and add hook useEditProfile.ts

--- src/components/profile/editProfile/editProfile.tsx
+++ src/components/profile/editProfile/editProfile.tsx
@@ -1,34 +1,17 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-
 import { Button, Card, Typography } from '../../ui'
 
 import s from './editProfile.module.scss'
 
-import { ControlledTextField } from '@/components'
-
-const schema = z.object({
-  name: z.string().min(1, 'Enter new name'),
-})
-
-type FormType = z.infer<typeof schema>
+import { ControlledTextField, EditProfileFormType, useEditProfile } from '@/components'
 
 type Props = {
   avatar: string
   prevName: string
-  onSubmit: (data: FormType) => void
+  onSubmit: (data: EditProfileFormType) => void
 }
 
 export const EditProfile = ({ avatar, prevName, onSubmit }: Props) => {
-  const { control, handleSubmit } = useForm<FormType>({
-    defaultValues: {
-      name: prevName,
-    },
-    resolver: zodResolver(schema),
-  })
-
-  const handleFormSubmitted = handleSubmit(onSubmit)
+  const { handleFormSubmitted, control } = useEditProfile(onSubmit, prevName)
 
   return (
     <Card className={s.card}>
