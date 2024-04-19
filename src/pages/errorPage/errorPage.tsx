19b5dbf905a8ba4e4ed04317f45a15bd2daa8c66
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-04-19 23:21:16 +0300

    correct 404 page

--- src/pages/errorPage/errorPage.tsx
+++ src/pages/errorPage/errorPage.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 import s from './errorPage.module.scss'
 
-import errorImg from '@/assets/images/icons/404.svg'
+import errorImg from '@/assets/images/404.svg'
 import { TypographyVariant } from '@/common'
 import { Button, NotFound, Typography } from '@/components'
 
@@ -11,7 +11,7 @@ export const ErrorPage = () => {
     <div className={s.container}>
       <NotFound className={s.wrapper}>
         <img src={errorImg} alt="404" width={450} height={200} />
-        <Typography as={'h1'} variant={TypographyVariant.body1}>
+        <Typography as={'h1'} variant={TypographyVariant.large}>
           Sorry! Page not found!
         </Typography>
         <Button as={NavLink} to={'/'}>
